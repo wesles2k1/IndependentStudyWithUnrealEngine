354c750721d574c73f13adc695388dcad511058a
wesles2k1 <wesleylpartch@gmail.com>
2023-09-08 13:37:14 -0400

    Added AnimalT Enum
    Added basic functionality into the animal selector widget (C++ object for processing has not been made yet)

---
+++ Source/LearningWithCPP/Public/CAnimalT.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+UENUM(BlueprintType)
+enum CAnimalT
+{
+	DEFAULT UMETA(DisplayName = "None"),
+
+	DOG	UMETA(DisplayName = "Dog"),
+	CAT UMETA(DisplayName = "Cat"),
+	HORSE UMETA(DisplayName = "Horse"),
+	FOX UMETA(DisplayName = "Fox")
+};
